4947708605fc333a486dce239ee0b7a6676f4bdd
Kalan Prudhomme <cleaver9@gmail.com>
2021-05-12 14:34:45 -0700

    added data sets, city test passing

---
+++ data/cities.js
@@ -0,0 +1,25 @@
+// example seed data
+const cities = [
+  
+  {
+    'place_id': '233174473',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'relation',
+    'osm_id': '4097196',
+    'boundingbox': [
+      '34.288802',
+      '34.614767',
+      '132.178545',
+      '132.696098'
+    ],
+    'lat': '34.3916058',
+    'lon': '132.4518156',
+    'display_name': 'Hiroshima, Hiroshima Prefecture, Japan',
+    'class': 'place',
+    'type': 'city',
+    'importance': 0.736336883362659,
+    'icon': 'https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png'
+  }
+];
+
+export default cities;
